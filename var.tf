c549b2c0b362e0d4f3ce4273526ed7e58c06d809
Nagiri Navya <navya.kongala@kyndryl.com>
2025-07-30 14:06:03 +0530

    repo

---
+++ var.tf
@@ -0,0 +1,11 @@
+ 
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+  default = "<subscription ID>"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "Azure Resource Group"
+  default = "resource group"
+ 
